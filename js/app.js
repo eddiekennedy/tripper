337fd5d8bbd4d80b57d0bc7722f0eb1709d74a4d
eddiekennedy <eddiekennedy@gmail.com>
2013-11-19 23:46:04 -0500

    refactored js to module pattern

--- js/app.js
+++ js/app.js
@@ -1,27 +1,51 @@
-var spreadsheetKey = '0AuYIWPWlXcROdG9wODB3aElJMUtLV1dYc2UtTzExcHc';
-var init = function( data ) {
-  // Map Setup
-  var map = new L.Map("map", {
-      center: new L.LatLng(42.3875, -71.1000),
-      zoom: 15
-  }).addLayer( new L.StamenTileLayer("terrain") );
-  // Item Loop
-  $.each(data, function( i, item ) {
-    var lookupLatLon = function(item) {
-      return 'http://nominatim.openstreetmap.org/search?format=json&q=' + item.address + '&callback=twist';
-    };
-    $.getJSON(lookupLatLon(item), function(data) {
-      //Add a marker to the map
-      var marker = L.marker([data[0].lat, data[0].lon]).addTo( map );
-      var popContent = [
-        '<p>' + item.name + '</p>',
-      ].join('');
-      marker.bindPopup( popContent );
+var app = (function() {
+  'use strict';
+
+  var module = {};
+
+  module._spreadsheeKey = '0AuYIWPWlXcROdG9wODB3aElJMUtLV1dYc2UtTzExcHc';
+
+  module.mapSetup = function() {
+    this.map = new L.Map("map", {
+        center: new L.LatLng(42.3875, -71.1000),
+        zoom: 15
+    }).addLayer( new L.StamenTileLayer("terrain") );
+  };
+
+  module.lookupUrl = function( address ) {
+    return 'http://nominatim.openstreetmap.org/search?format=json&q=' + address + '&callback=twist';
+  };
+
+  module.fetchData = function() {
+    Tabletop.init({
+      key: String(module._spreadsheeKey),
+      callback: function(data, tabletop) { module.addToMap( data ); },
+      simpleSheet: true
     });
-  });
-};
-Tabletop.init({
-  key: spreadsheetKey,
-  callback: function(data, tabletop) { init(data); },
-  simpleSheet: true
-});
+  };
+
+  module.addToMap = function( data ) {
+    $.each(data, function( i, item ) {
+      $.getJSON( module.lookupUrl(item.address), function(data) {
+        var marker = L.marker([data[0].lat, data[0].lon]).addTo( module.map );
+        var popContent = [
+          '<div class="detail-card">',
+            '<h3>' + item.name + '</h3>',
+            '<address>' + item.address + '</address>',
+            '<address><a href="' + item.website + '">' + item.website.replace('http://','') + '</a></address>',
+            '<div class="categories">' + item.categories + '</div>',
+          '</div>'
+        ].join('');
+        marker.bindPopup( popContent );
+      });
+    });
+  };
+
+  module.init = function() {
+    this.mapSetup();
+    this.fetchData();
+  };
+
+  module.init();
+
+})();
